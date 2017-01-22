dbf55034dda680cb282caad50a87b1dfc6d2d2b3
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-01-21 19:08:26 -0800

    use youtube-search gem and make search button work

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,13 @@
 class SearchesController < ApplicationController
   def create
-    # do the search
+    @word = Word.find(params[:word_id])
+    search = YouTube::Search.new(@word.word)
+    search.first_page!
+    # save search items
+    @word.searches.delete_all
+    search.get_search_items.each do |item|
+      @word.searches.create(result: item.title)
+    end
+    redirect_to word_path(@word)
   end
 end
