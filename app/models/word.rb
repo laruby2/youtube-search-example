c2fb91cf1049a348b060e36f30cd5eb8e486e381
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-01-29 15:23:17 -0800

    Refactor: pull out search_and_save_results to model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,12 @@
 class Word < ApplicationRecord
   has_many :searches, dependent: :destroy
+
+  def search_and_save_results
+    search = YouTube::Search.new(word)
+    search.first_page!
+    searches.delete_all
+    search.get_search_items.each do |item|
+      searches.create(result: item.title)
+    end
+  end
 end
