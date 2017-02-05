1651e66dcf17a28a3d67704c3a61c081caf2e19b
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-02-04 16:32:47 -0800

    Refactor results create into words search action

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,2 @@
 class ResultsController < ApplicationController
-  def create
-    @word = Word.find(params[:word_id])
-    @word.search_and_save_results
-    redirect_to word_path(@word)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :words do
+    post 'search', on: :member
     resources :results
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
